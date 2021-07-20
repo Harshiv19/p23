865231ae9123d161218f795da1f32338d4380ce4
Harshiv19 <75972903+Harshiv19@users.noreply.github.com>
2021-07-21 10:52:22 +1200

    Add files via upload

---
+++ project-23b-template-main/helicopter.js
@@ -0,0 +1,18 @@
+class Helicopter {
+    constructor(x,y,width,height) {
+
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+
+      this.image = loadImage("sprites/helicopter.png");
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill(255);
+      rect(pos.x, pos.y, this.width, this.height);
+      
+    }
+  };
